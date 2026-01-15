e540a703130d76c82fae08cdfd6ede75f02343ae
Sarthak Pawar <sarthakpawar0294@gmail.com>
2026-01-15 21:48:57 +0530

    Refactor main.tsx to use multiple context providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext.tsx';
+import { CartProvider } from './context/CartContext.tsx';
+import { ThemeProvider } from './context/ThemeContext.tsx'; // Import
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AuthProvider>
+      <CartProvider>
+        <ThemeProvider> {/* Add Provider */}
+          <App />
+        </ThemeProvider>
+      </CartProvider>
+    </AuthProvider>
+  </StrictMode>,
 );
